9950e098c1d2507da652ca6c3706720f28711a1f
guttinue <96751844+guttinue@users.noreply.github.com>
2024-12-05 14:31:08 -0300

    Delete InFact_validacao_login_cadastro directory

--- InFact_validacao_login_cadastro/projeto_cad_usuarios/app_cad_usuarios/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=255)
-    matricula = models.TextField(max_length=255)
-    email = models.TextField(max_length=255, )
-    senha = models.TextField(max_length=255)
-
